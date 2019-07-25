b9d557418b66a5d2fc0416b45e10a69624bf69cc
Руднев Геннадий Владимирович <grudnev@systematica-consulting.ru>
2019-07-25 17:51:37 +0300

    add day 5

---
+++ day_5/src/main/java/com/example/day_5/Day_5_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.day_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Day_5_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_day_5_)
+    }
+}
